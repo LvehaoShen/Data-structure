698e24a51d43f57a0c681e713e1302fa4e6476aa
graph-shen <53935713+graph-shen@users.noreply.github.com>
2019-12-08 14:46:14 +0800

    Update queue.h

--- Queue/queue.h
+++ Queue/queue.h
@@ -1,16 +1,20 @@
 #pragma once
 #include<stdbool.h>
-typedef struct Node Node;
-typedef struct Node* PtrToNode;
-typedef PtrToNode Queue;
-typedef PtrToNode Position;
-struct Node {
+typedef struct QueueNode QueueNode;
+typedef struct QueueNode* PtrToNode;
+typedef struct Queue Queue;
+typedef struct Queue* PtrToQueue;
+struct QueueNode {
 	int ele;
-	Position next;
+	PtrToNode next;
 };
-Queue MakeEmpty(Queue L);
-bool IsEmpty(Queue L);
-Queue CreateQueue(Queue L);
-Queue Enqueue(Queue L);
-Queue Dequeue(Queue L);
-Queue FindNode(Queue L, int i);
+struct Queue {	
+	PtrToNode front;
+	PtrToNode rear;
+};
+PtrToQueue MakeEmpty(PtrToQueue L);
+bool IsEmpty(PtrToQueue L);
+PtrToQueue CreateQueue(PtrToQueue L);
+PtrToQueue Enqueue(PtrToQueue L);
+PtrToNode Dequeue(PtrToQueue L);
+PtrToNode FindNode(PtrToQueue L, int i);
