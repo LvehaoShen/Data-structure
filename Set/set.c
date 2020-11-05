c7d287101ea4bb2e1ea1841ac68c8b329ef7622c
graph-shen <53935713+graph-shen@users.noreply.github.com>
2020-11-05 14:54:28 +0800

    Update set.c

--- Set/set.c
+++ Set/set.c
@@ -8,10 +8,12 @@ void Init(set S)
 
 	int i;
 	for (i = 10; i > 0; i--)
-		S[i] = -1;
+		S[i] = -1; //数组中存储的元素：是高度的负值再减去1
 }
-void Setunion(set S, SetType root1, SetType root2)//root1和root2是根节点
+void Setunion(set S, SetType root1, SetType root2) //root1和root2是根节点
 {
+	if (Find(root1, S) == Find(root2, S))
+		return;
 	if (S[root2] < S[root1])
 		S[root1] = root2;
 	else
